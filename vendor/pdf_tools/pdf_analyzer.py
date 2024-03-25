cad78958b65151b5312b0fb13b4473820e6bedcb
Abduraimov Muminjon <muminjonguru@outlook.com>
2024-03-25 18:07:08 +0000

    current user path and template paths

--- vendor/pdf_tools/pdf_analyzer.py
+++ vendor/pdf_tools/pdf_analyzer.py
@@ -6,11 +6,19 @@ def scan_pdf(directory):
         for file in files:
             if file.lower().endswith('.pdf'):
                 full_path = os.path.join(root, file)
+                # Call pdfid.py script here
                 result = subprocess.run(['python', 'pdfid.py', full_path], capture_output=True, text=True)
                 if 'JavaScript' in result.stdout or 'EmbeddedFile' in result.stdout:
                     print(f"WARNING: The file {full_path} contains JavaScript or an embedded file.")
 
+# Get the username from the environment variables
+user_name = os.getenv('USERNAME')
+
 # Main directories to scan
-directories = ['downloads', 'downloads/documents']
+directories = [
+    f'C:\\Users\\{user_name}\\Downloads',
+    f'C:\\Users\\{user_name}\\Downloads\\Documents'
+]
+
 for directory in directories:
     scan_pdf(directory)
