02d540a62f7dcdce143db4a11b2ca708267e007c
Abduraimov Muminjon <muminjonguru@outlook.com>
2024-04-19 09:41:58 +0100

    file removing upgraded with os.chmod
    if file is not possible to remove, now it changes the permissions and try to remove the infected file again.

--- vendor/clamav/clamdaemon.py
+++ vendor/clamav/clamdaemon.py
@@ -1,32 +1,43 @@
 import csv
 import pyclamd
 import os
+import time
+import stat
 
 def scan_directory(directory_path):
     try:
-        # Initialize the clamd connection
         cd = pyclamd.ClamdNetworkSocket('127.0.0.1', 3310)
-
-        # Scan the entire directory
         result = cd.multiscan_file(directory_path)
-
-        # Handle the scan results
         if result:
             print(f"Malware Found in {directory_path}:")
             for infected_file, virus_name in result.items():
                 print(f"{infected_file}: {virus_name}")
-                # Attempt to delete the infected file
-                try:
-                    os.remove(infected_file)
+                if attempt_delete(infected_file):
                     print(f"Deleted {infected_file} as it was infected with {virus_name}.")
-                except PermissionError:
-                    print(f"Failed to delete {infected_file}. Permission denied.")
-                except Exception as e:
-                    print(f"Failed to delete {infected_file}. Error: {e}")
+                else:
+                    print(f"Failed to delete {infected_file}.")
         else:
             print(f"No malware detected in {directory_path}.")
     except Exception as e:
         print(f"An error occurred while scanning {directory_path}: {e}")
+
+def attempt_delete(file_path):
+    """Attempt to delete a file, changing permissions if necessary and retrying on failure."""
+    try:
+        os.remove(file_path)
+        return True
+    except PermissionError:
+        # Change file permissions and try again
+        os.chmod(file_path, stat.S_IWUSR)
+        try:
+            os.remove(file_path)
+            return True
+        except Exception as e:
+            print(f"Retry failed: {e}")
+            return False
+    except Exception as e:
+        print(f"Initial delete failed: {e}")
+        return False
 
 def read_directories_from_csv(file_path):
     directories = []
@@ -38,7 +49,7 @@ def read_directories_from_csv(file_path):
     return directories
 
 # Example usage
-csv_file_path = 'D:\\CyberKombat RT\\config\\directories.csv'  # Update this path to your actual CSV file location
+csv_file_path = 'D:\\CyberKombat RT\\config\\directories.csv'
 directories_to_scan = read_directories_from_csv(csv_file_path)
 
 for directory in directories_to_scan:
